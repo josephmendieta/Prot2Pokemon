7259db8b4cdb0c0147499f05403a572e082fe66f
josephmendieta <jyma1204@gmail.com>
2024-04-02 03:19:07 -0500

    Segundo Commit
    Funciona a la perfección, sólo hace falta el botón de inicio, el error 404 y el comando de voz

--- js/main.js
+++ js/main.js
@@ -5,9 +5,24 @@ $(document).ready(function () {
             type: "GET",
             contentType: "application/json",
             success: function (data) {
-                console.log(data.sprites.other.home.front_default)
                 $("#imagen_pokemon").html('<img src="' + data.sprites.other.home.front_default + '">');
-//quedé en el minuto 43:25
+                $("#nombre-pokemon").text(data.name.toUpperCase());
+                $("#pokemon-info").html(`
+                        <p><strong>Altura:</strong> ${data.height / 10} m</p>
+                        <p><strong>Peso:</strong> ${data.weight / 10} kg</p>
+                        <p><strong>Tipo:</strong> ${data.types.map(type => type.type.name).join(", ")}</p>
+                        <p><strong>Habilidades:</strong> ${data.abilities.map(ability => ability.ability.name).join(", ")}</p>
+                        <p><strong>Experiencia Base:</strong> ${data.base_experience}</p>
+                        <p><strong>Estadísticas:</strong></p>
+                        <ul>
+                            ${data.stats.map(stat => `<li><strong>${stat.stat.name}:</strong> ${stat.base_stat}</li>`).join("")}
+                        </ul>
+                        <p><strong>Formas:</strong> ${data.forms.map(form => form.name).join(", ")}</p>
+                        <p><strong>Movepool:</strong></p>
+                        <ul>
+                            ${data.moves.slice(0, 5).map(move => `<li>${move.move.name}</li>`).join("")}
+                        </ul>
+                    `);
             }
         })
     })
